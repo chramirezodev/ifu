558ea6b4bb047ce7b849ee7d95f8a6efaa8ba437
Tu Nombre <tu.email@ejemplo.com>
2025-04-25 11:38:57 -0500

    feat: actualiza información de contacto y agrega iconos

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,26 +2,26 @@ import { EmailIcon, PhoneIcon, ClockIcon, FacebookIcon, InstagramIcon, WhatsAppI
 
 // Información de contacto de la empresa
 export const contactInfo = {
-  email: 'info@immigrationforus.com',
-  phone: '+1 (123) 456-7890',
-  whatsapp: '+1 (123) 456-7890',
-  whatsappNumber: '11234567890',
-  address: '123 Main St, New York, NY 10001',
-  googleMapsUrl: 'https://goo.gl/maps/example',
-  mapUrl: 'https://www.google.com/maps/embed?example',
+  email: 'cpalisa@immigrationfor-us.com',
+  phone: '+1 (954) 588-4018',
+  whatsapp: '+1 (954) 588-4018',
+  whatsappNumber: '19545884018',
+  address: '2200 N Federal Hwy, Boca Raton, FL 33431',
+  googleMapsUrl: 'https://goo.gl/maps/YourActualMapLink',
+  mapUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3581.971342687913!2d-80.1350047!3d26.1312066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d902d86a301a15%3A0xd3ce86aa2ed6ac7a!2s2200%20N%20Federal%20Hwy%2C%20Boca%20Raton%2C%20FL%2033431%2C%20EE.%20UU.!5e0!3m2!1ses!2sar!4v1657561487018!5m2!1ses!2sar',
   workHours: 'Lunes a Viernes: 9:00 AM - 5:00 PM',
   methods: [
     {
       icon: EmailIcon,
       title: 'Email',
-      info: 'info@immigrationforus.com',
-      link: 'mailto:info@immigrationforus.com'
+      info: 'cpalisa@immigrationfor-us.com',
+      link: 'mailto:cpalisa@immigrationfor-us.com'
     },
     {
       icon: PhoneIcon,
       title: 'Teléfono',
-      info: '+1 (123) 456-7890',
-      link: 'tel:+11234567890'
+      info: '+1 (954) 588-4018',
+      link: 'tel:+19545884018'
     },
     {
       icon: ClockIcon,
@@ -33,15 +33,15 @@ export const contactInfo = {
   socialMedia: [
     {
       icon: FacebookIcon,
-      link: 'https://facebook.com/immigrationforus'
+      link: 'https://www.facebook.com/immigration.for.us'
     },
     {
       icon: InstagramIcon,
-      link: 'https://instagram.com/immigrationforus'
+      link: 'https://www.instagram.com/immigration.for.us'
     },
     {
       icon: WhatsAppIcon,
-      link: 'https://wa.me/11234567890'
+      link: 'https://wa.me/19545884018'
     }
   ]
 };
@@ -49,6 +49,5 @@ export const contactInfo = {
 // Redes sociales
 export const socialMedia = {
   facebook: "https://www.facebook.com/immigration.for.us",
-  instagram: "https://www.instagram.com/immigration.for.us",
-  twitter: "https://twitter.com/immigrationforu"
+  instagram: "https://www.instagram.com/immigration.for.us"
 }; 
